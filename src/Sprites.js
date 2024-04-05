68f9f0b3b81cce3bf4fea366efcfbd5351860b2e
Franco Werner <ifrank44445@gmail.com>
2024-04-05 18:01:34 -0300

    Sprites

--- src/Sprites.js
+++ src/Sprites.js
@@ -5,17 +5,40 @@ const ctx = canvans.getContext("2d");
 //Se debera pasar dentro del class para que tome el valor mas actual.
 
 class Sprites {
-    constructor(col, row, models) {
+    constructor({ col, row, models = {}, scale, img }) {
         this.width = canvans / col
         this.height = canvans / row
+        this.scale = scale
         this.currentSprite = {
             type: "",
-            col: 0
+            stage: 0
         }
+        this.img = (() => {
+            const i = new Image()
+            return i.src = img
+        })()
         this.models = models
     }
 
-    injectSprit({ type }) {
+    handlerSprite({ type }) {
+
+        const compare = this.currentSprite.type !== type
+
+        const currentModel = this.models[type]
+
+        if (compare) {
+            this.currentSprite = currentModel
+        } else {
+            const nextStage = this.compareSprite.stage + 1
+            this.currentSprite.stage = nextStage > currentModel.col ? 0 : nextStage
+        }
+    }
+
+    injectSprite({ type }) {
+
+        ctx.drawImage(
+
+        )
 
     }
 
